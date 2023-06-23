de963dd77bc7dc94a4344165fff047c182e3a0f5
Whatsuf148 <whatsufsachin@gmail.com>
2023-06-23 14:09:40 +0545

    Form validation

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -11,98 +11,121 @@ class LogIn extends StatefulWidget {
 class _LogInState extends State<LogIn> {
   String name = "";
   bool checkbutton = false;
+  final _formkey = GlobalKey<FormState>();
+
+  GotoHome(BuildContext context) async {
+    if (_formkey.currentState!.validate()) {
+      setState(() {
+        checkbutton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homepage);
+      setState(() {
+        checkbutton = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 60,
-            ),
-            Image.asset(
-              "assets/images/login.png",
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 5.00,
-            ),
-            Text(
-              "Welcome $name",
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Enter your Username",
-                      labelText: "Username",
-                    ),
-                    onChanged: (value) {
-                      name = value;
-                      setState(() {});
-                    },
+      child: SingleChildScrollView(
+        child: Center(
+          child: Form(
+            key: _formkey,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 60,
+                ),
+                Image.asset(
+                  "assets/images/login.png",
+                  fit: BoxFit.cover,
+                ),
+                const SizedBox(
+                  height: 5.00,
+                ),
+                Text(
+                  "Welcome $name",
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Enter Password",
-                      labelText: "Password",
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  InkWell(
-                    onTap: () async {
-                      setState(() {
-                        checkbutton = true;
-                      });
-                      await Future.delayed(Duration(seconds: 1));
-                      Navigator.pushNamed(context, MyRoutes.homepage);
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(seconds: 1),
-                      height: 50,
-                      width: checkbutton ? 50 : 150,
-                      alignment: Alignment.center,
-                      child: checkbutton
-                          ? Icon(
-                              Icons.done,
-                              color: Colors.white,
-                            )
-                          : Text(
-                              "Login",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                              ),
-                            ),
-                      decoration: BoxDecoration(
-                        color: Colors.deepPurple,
-                        borderRadius:
-                            BorderRadius.circular(checkbutton ? 50 : 8),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Enter your Username",
+                          labelText: "Username",
+                        ),
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Username can't be empty";
+                          }
+                          return null;
+                        },
                       ),
-                    ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          hintText: "Enter Password",
+                          labelText: "Password",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password can't be empty";
+                          }
+                          if (value.length < 6) {
+                            return "Your Password should be more than 6";
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      InkWell(
+                        onTap: () => GotoHome(context),
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          height: 50,
+                          width: checkbutton ? 50 : 150,
+                          alignment: Alignment.center,
+                          child: checkbutton
+                              ? Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : Text(
+                                  "Login",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius:
+                                BorderRadius.circular(checkbutton ? 50 : 8),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  // ElevatedButton(
-                  //     onPressed: () {
-                  //       Navigator.pushNamed(context, MyRoutes.homepage);
-                  //     },
-                  //     style: const ButtonStyle(
-                  //         minimumSize: MaterialStatePropertyAll(Size(150, 50))),
-                  //     child: const Text("Login")),
-                ],
-              ),
-            )
-          ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
